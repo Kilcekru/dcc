af2628b88a9e3939635986054fcab037f6402bb3
Jürgen Dressel <jdressel@dcodeit.com>
2023-12-24 10:57:03 +0100

    refactor(campaign): sam WIP

--- packages/workers/campaign/src/ecs/entities/SAM.ts
+++ packages/workers/campaign/src/ecs/entities/SAM.ts
@@ -1,6 +1,9 @@
 import * as DcsJs from "@foxdelta2/dcsjs";
+import * as Types from "@kilcekru/dcc-shared-types";
+import * as Utils from "@kilcekru/dcc-shared-utils";
 
-import { MapEntity, MapEntityProps } from "./_base/MapEntity";
+import { world } from "..";
+import { MapEntity, MapEntityProps } from "./_base";
 import { Objective } from "./Objective";
 
 export interface SAMProps extends Omit<MapEntityProps, "entityType"> {
@@ -22,5 +25,44 @@ export class SAM extends MapEntity {
 		this.range = args.range;
 		this.objective = args.objective;
 		this.type = args.type;
+	}
+
+	static create(args: SAMProps) {
+		return new SAM(args);
+	}
+
+	static generate(args: { coalition: DcsJs.Coalition; objectivePlans: Array<Types.Campaign.ObjectivePlan> }) {
+		const strikeTargets = world.dataStore?.strikeTargets;
+
+		if (strikeTargets == null) {
+			throw new Error("strikeTargets not found");
+		}
+
+		for (const plan of args.objectivePlans) {
+			// Get only the plans with SAMs
+			const withSam = plan.groundUnitTypes.some((gut) => gut === "sam");
+
+			if (!withSam) {
+				continue;
+			}
+
+			// Get the targets for the plan
+			const targets = strikeTargets[plan.objectiveName];
+
+			if (targets == null) {
+				continue;
+			}
+
+			// Select a SAM target
+			const samTargets = targets.filter((target) => target.type === "SAM");
+
+			const selectedSamTarget = Utils.Random.item(samTargets);
+
+			if (selectedSamTarget == null) {
+				continue;
+			}
+
+			// Create the SAM
+		}
 	}
 }
